bfe5504c87d5f433579daa74e7d81937a5b94ccd
Ankita Satyavarapu <ankis@umich.edu>
2020-04-09 01:48:29 -0400

    pictures, characters

--- script.js
+++ script.js
@@ -27,30 +27,43 @@ var resultView = new Vue({
     textScrollSpeed: 10,
     currently_scrolling: false,
 
-    background_img: ["images/winter_diag.jpg", "images/1.jpg", "images/2.jpg", "images/3.jpg"],
-
+    background_img: ["images/winter_diag.jpg", "images/cctcbackground.jpg", "images/blueBus.jpg", "images/pierpont.jpg", "images/pierpontPanda.jpg", "images/pandaServing.jpg", "images/pandaCashier.jpg", "images/pandaFood.webp", "images/pandaBag.jpg", "images/outsideFight.png", "images/inFight.png", "images/snowball.jpg", "images/pingpongBBB.jpg", "images/ggbrown.jpg"],
+    character_img: ["images/billy_magic.png", "images/noCharacter.png", "images/snowballCharacter.png", "images/throwingCharacter.png"],
     scene: [
-      {background: 0, speaker: "Narrator", nextScene: 1, story: "It's another cold day at the University of Michigan. You are a new freshman ready to start your first college experience. How will this new environment treat you?"},
-      {background: 0, speaker: "Narrator", nextScene: 2, story: "Out of the corner of your eye, you see a person approach you. Who could it be?"},
-      {background: 0, speaker: "???", nextScene: 3, story: "Hey! How’s it going? Are you a new student here? I saw you walking around and you looked a bit confused as to where to go."},
-      {background: 0, speaker: "You", nextScene: 4, story: "(wow, he looks like he works out a lot) Yeah, this is my first day on campus, so I’m a bit lost. I can’t figure out how to get to the STAMPS building. What’s your name?"},
-      {background: 0, speaker: "Billy", nextScene: 5, story: "I’m Billy Magic-"},
-      {background: 0, speaker: "Unknown Voice", nextScene: 6, story: "BILLY MAGIC"},
-      {background: 0, speaker: "Billy", nextScene: 7, story: "-an upperclassman here at Michigan! Let me introduce you to… Magic Bus!"},
-      {background: 0, speaker: "You", nextScene: 8, story: "(where did that loud voice come from? weird.)"},
-      {background: 0, speaker: "Billy", nextScene: 9, story: "This is the Central Campus Transit Center, also known as CC Little. All the blue buses, which are the university buses that take you around campus, stop here. And, they’re free!"},
-      {background: 0, speaker: "You", nextScene: 10, story: "Nice to meet you Billy, I’m [PLAYER NAME]. That’s the bus system right? Which one should I take to get to north campus?"},
-      {background: 0, speaker: "Billy", nextScene: 11, story: "Well, you have a lot of options. I would recommend Bursley-Baits or Northwood, those are usually the fastest! Anyways, I’ve got to head to a bus convention soon, but let’s meet up again later so I can show you around a bit more! *wink*"},
-      {background: 0, speaker: "You", nextScene: 12, story: "Thanks a lot Billy, I’ll see you later!"},
-      {background: 0, speaker: "Narrator", nextScene: 13, story: "You get on Bursley-Baits and find yourself a seat. After a rather uneventful ride, you find yourself on North Campus."},
-      {background: 0, speaker: "You", nextScene: 14, story: "(hmm, I have a lot of time before my first class, and I’m kind of hungry, maybe I should get some food)"},
-      {background: 0, speaker: "Narrator", nextScene: 15, story: "In the corner of your eye, you spot a restaurant called Pamda Express. "},
-      {background: 0, speaker: "You", nextScene: 16, story: "(wow, that looks good! I think I’ll eat there)"},
-      {background: 0, speaker: "Narrator", nextScene: 17, story: "You order a bowl of fried rice and orange chicken and make your way to the checkout."},
-      {background: 0, speaker: "Pamda Express Employee", nextScene: 18, story: "...Drink? Donation?"},
-      {background: 0, speaker: "You", button: [{text: "Yes", goto: 19}, {text: "No", goto: 20}]},
-      {background: 0, speaker: "You", nextScene: 21, story: "Sure, round it up please."},
-      {background: 0, speaker: "You", nextScene: 21, story: "No thank you.", is_final: true}
+      {background: 0, character: 1, speaker: "Narrator", nextScene: 1, story: "It's another cold day at the University of Michigan. You are a transfer student ready to start your first day at Umich. How will this new environment treat you?"},
+      {background: 0, character: 1, speaker: "Narrator", nextScene: 2, story: "Out of the corner of your eye, you see a person approach you. Who could it be?"},
+      {background: 0, character: 0, speaker: "???", nextScene: 3, story: "Hey! How's it going? Are you a new student here? I saw you walking around and you looked a bit confused as to where to go."},
+      {background: 0, character: 0, speaker: "You", nextScene: 4, story: "(wow, he looks like he works out a lot) Yeah, this is my first day on campus, so I'm a bit lost. I can't figure out how to get to the STAMPS building. What's your name?"},
+      {background: 0, character: 0, speaker: "Billy", nextScene: 5, story: "I'm Billy Magic-"},
+      {background: 0, character: 0, speaker: "Unknown Voice", nextScene: 6, story: "BILLY MAGIC"},
+      {background: 0, character: 0, speaker: "Billy", nextScene: 7, story: "-an upperclassman here at Michigan! Let me introduce you to… Magic Bus!"},
+      {background: 0, character: 0, speaker: "You", nextScene: 8, story: "(where did that loud voice come from? weird.)"},
+      {background: 1, character: 0, speaker: "Billy", nextScene: 9, story: "This is the Central Campus Transit Center, also known as CC Little. All the blue buses, which are the university buses that take you around campus, stop here. And, they're free!"},
+      {background: 1, character: 0, speaker: "You", nextScene: 10, story: "Nice to meet you Billy. That's the bus system right? Which one should I take to get to north campus?"},
+      {background: 1, character: 0, speaker: "Billy", nextScene: 11, story: "Well, you have a lot of options. I would recommend Bursley-Baits or Northwood, those are usually the fastest, you can always check the app Magic Bus to see which one is arriving soonest - definitely useful in these cold times! Anyways, I've got to head to a bus convention soon, but let's meet up again later so I can show you around a bit more! *wink*"},
+      {background: 1, character: 0, speaker: "You", nextScene: 12, story: "Thanks a lot Billy, I'll see you later!"},
+      {background: 2, character: 1, speaker: "Narrator", nextScene: 13, story: "You get on Bursley-Baits and find yourself a seat. After a rather uneventful ride, you find yourself on North Campus."},
+      {background: 3, character: 1, speaker: "You", nextScene: 14, story: "(hmm, I have a lot of time before my first class, and I'm kind of hungry, maybe I should get some food)"},
+      {background: 4, character: 1, speaker: "Narrator", nextScene: 15, story: "In the corner of your eye, you spot a restaurant called Panda Express. "},
+      {background: 4, character: 1, speaker: "You", nextScene: 16, story: "(wow, that looks good! I think I'll eat there)"},
+      {background: 5, character: 1, speaker: "Narrator", nextScene: 17, story: "You order a bowl of fried rice and orange chicken and make your way to the checkout."},
+      {background: 6, character: 1, speaker: "Panda Express Employee", nextScene: 18, story: "...Drink? Donation?"},
+      {background: 6, character: 1, speaker: "You", button: [{text: "Yes", goto: 19}, {text: "No", goto: 20}]},
+      {background: 6, character: 1, speaker: "You", nextScene: 21, story: "Sure, round it up please."},
+      {background: 6, character: 1, speaker: "You", nextScene: 21, story: "No thank you."},
+      {background: 7, character: 1, speaker: "You", nextScene: 22, story: "This look so great ... can't wait to dig in!"},
+      {background: 8, character: 1, speaker: "You", nextScene: 23, story: "Wow, what a great meal .. I'm so full, let me pack the rest to go. "},
+      {background: 9, character: 1, speaker: "Narrator", nextScene: 24, story: "You look out of the window to see a snowball fight going on."},
+      {background: 9, character: 1, speaker: "You", nextScene: 25, story: "I still have so much time to kill. Seems like there's a snowball fight going on outside.. Maybe I'll join! "},
+      {background: 10, character: 1, speaker: "Narrator", nextScene: 26, story: "You walk over to the people snowball fighting in front of the BBB"},
+      {background: 10, character: 2, speaker: "Snowball Fight Player", nextScene: 27, story: "Hey, do you want to join us?"},
+      {background: 10, character: 2, speaker: "You", nextScene: 28, story: "Of course!", },
+      {background: 10, character: 3, speaker: "Snowball Fight Player", nextScene: 29, story: "Be careful what you wish for!", },
+      {background: 11, character: 1, speaker: "You", nextScene: 30, story: "Whoa!!", },
+      {background: 10, character: 2, speaker: "Snowball Fight Player", nextScene: 31, story: "Hahhaha, my name is Carton. I'm actually getting pretty cold, do you want to go into the BBB and play some pingpong with me?", },
+      {background: 10, character: 2, speaker: "You", nextScene: 32, story: "Yeah sure", },
+      {background: 12, character: 1, speaker: "You", nextScene: 33, story: "Shoot! I forgot about class!! Sorry Calton, I gotta go ", },
+      {background: 13, character: 1, speaker: "You", nextScene: 33, story: "Ah, finally GG Brown - can't wait to take my first Umich EECS course", },
     ],
 
     // storyText: {
@@ -75,9 +88,11 @@ var resultView = new Vue({
       console.log(this.playerInfo)
       this.main_menu = false;
       this.playerInfo = true;
+      console.log(this.playerName);
     },
 
     startGame() {
+        console.log(this.playerName);
       this.game_window = true;
       this.playerInfo = false;
       this.current_scene = 0;
@@ -98,6 +113,11 @@ var resultView = new Vue({
       return "'" + this.background_img[this.scene[this.current_scene].background] + "'";
     },
 
+    getCharacterString() {
+        // return "'images/" + this.current_scene + ".jpg'";
+        return this.character_img[this.scene[this.current_scene].character];
+      },
+
     goToScene(index) {
       console.log(index);
       console.log(this.scene[this.current_scene].button[index].goto);
@@ -105,6 +125,7 @@ var resultView = new Vue({
     },
 
     nextScene() {
+        console.log(this.playerName);
       // If text is currently scrolling, clicking the textbox again will show the entire 
       if (this.currently_scrolling) {
         console.log("done");
@@ -130,7 +151,7 @@ var resultView = new Vue({
       var _this = this;
       _this.currently_scrolling = true;
       var addChars = setInterval(function() {
-        if (i < txt.length && _this.currently_scrolling) {
+        if (txt!=undefined && i < txt.length && _this.currently_scrolling ) {
           curr_txt += txt.charAt(i);
           $(".mainText").text(curr_txt);
           i++;
